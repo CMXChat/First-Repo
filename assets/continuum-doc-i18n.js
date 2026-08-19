5e47ba30d7f328da6684eca16b120e16c0733140
CMXChat <133807494+CMXChat@users.noreply.github.com>
2026-08-19 14:06:23 -0400

    Make Continuum doc final refinement loader deterministic

--- assets/continuum-doc-i18n.js
+++ assets/continuum-doc-i18n.js
@@ -19,7 +19,7 @@
   if (!document.querySelector('script[data-continuum-knowledge-time]')) {
     const script = document.createElement('script');
     script.src = '/assets/continuum-doc-knowledge-time.js?v=20260819-2';
-    script.defer = true;
+    script.async = false;
     script.dataset.continuumKnowledgeTime = 'loader';
     document.body.append(script);
   }
